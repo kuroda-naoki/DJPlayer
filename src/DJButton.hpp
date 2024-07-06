60de1629a09b59f20814ea25dfe6bc9758f9d892
Naoki-Kuroda-4196 <2200418@ecccomp.com>
2024-07-06 09:11:46 +0900

    [add] ボタンピンを追加、動作確認済み

--- src/DJButton.hpp
+++ src/DJButton.hpp
@@ -1,1 +1,6 @@
 #pragma once
+
+#define DJ_BUTTON_PIN_1 12
+#define DJ_BUTTON_PIN_2 13
+#define DJ_BUTTON_PIN_3 21
+#define DJ_BUTTON_PIN_4 0
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 
 #include "DJBeepTimer.hpp"
 #include "DJDiscEncoder.hpp"
+#include "DJButton.hpp"
 
 void setup()
 {
 
+  Serial.begin(115200);
+
   pinMode(MOSQUIT_PIN, OUTPUT);
   pinMode(ENCODER_PIN, INPUT);
+
+  pinMode(DJ_BUTTON_PIN_1, INPUT);
+  pinMode(DJ_BUTTON_PIN_2, INPUT);
+  pinMode(DJ_BUTTON_PIN_3, INPUT);
+  pinMode(DJ_BUTTON_PIN_4, INPUT);
 
   digitalWrite(MOSQUIT_PIN, LOW);
 
@@ -23,6 +31,42 @@ void setup()
 
 void loop()
 {
+
+  if (digitalRead(DJ_BUTTON_PIN_1) == HIGH)
+  {
+    Serial.println("DJ_BUTTON_PIN_1");
+    timerStart(DJTimer);
+    delay(10);
+    timerStop(DJTimer);
+    digitalWrite(MOSQUIT_PIN, LOW);
+  }
+  
+  if (digitalRead(DJ_BUTTON_PIN_2) == HIGH)
+  {
+    Serial.println("DJ_BUTTON_PIN_2");
+    timerStart(DJTimer);
+    delay(10);
+    timerStop(DJTimer);
+    digitalWrite(MOSQUIT_PIN, LOW);
+  }
+
+  if (digitalRead(DJ_BUTTON_PIN_3) == HIGH)
+  {
+    Serial.println("DJ_BUTTON_PIN_3");
+    timerStart(DJTimer);
+    delay(10);
+    timerStop(DJTimer);
+    digitalWrite(MOSQUIT_PIN, LOW);
+  }
+
+  // if (digitalRead(DJ_BUTTON_PIN_4) == HIGH)
+  // {
+  //   Serial.println("DJ_BUTTON_PIN_4");
+  //   timerStart(DJTimer);
+  //   delay(10);
+  //   timerStop(DJTimer);
+  //   digitalWrite(MOSQUIT_PIN, LOW);
+  // }
 
   if (isScratched())
   {
